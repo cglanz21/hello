d3e8382d92187732e12750dae659d8456d5569d2
Cole Glanz <cglanz2@huskers.unl.edu>
2022-01-28 15:14:42 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Cole Glanz
+ * Date: 2022
+ * 
+ * Hello World tutorial program
+ * Moddified to State Name and Major
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Cole Glanz\n");
+     printf("Computer Engineering\n");
+     
+     return 0;
+     
+ }
