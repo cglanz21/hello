6c2d4480b304394f6c53c90c8c2c330cd2ecf829
William Thompson <wthompson11@huskers.unl.edu>
2022-01-28 16:22:33 +0000

    William Thompson added his name and major

--- hello.c
+++ hello.c
@@ -1,18 +1,20 @@
 /**
  * Author: Cole Glanz
  * Date: 2022
- * 
+ *
  * Hello World tutorial program
  * Moddified to State Name and Major
  */
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
-     
+
      printf("Cole Glanz!git\n");
      printf("Computer Engineering\n");
-     
+     printf("William Thompson!\n");
+     printf("Computer Engineering\n");
+
      return 0;
-     
+
  }
